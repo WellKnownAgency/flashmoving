f99acef624088b367d0271e1bd2953212eef4a69
Taras <taraskim@wknown.com>
2018-10-08 17:35:39 -0400

    Started Project / Created Navbar

---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light">
+  <a class="navbar-brand" href="#">
+    <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+    Bootstrap
+  </a>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'PageController@index');
+Route::get('/family', 'PageController@family');
