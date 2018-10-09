2ecadee1e9b403631feb195a447ac4f89c53ae57
Taras <taraskim@wknown.com>
2018-10-09 11:07:17 -0400

    Html Markup for "Family" Page

--- resources/views/family.blade.php
+++ resources/views/family.blade.php
@@ -6,5 +6,59 @@
 @extends('main')
 
 @section('content')
+<!-- Header starts -->
+<header>
 
+</header>
+<!--End of Header -->
+
+<!--Section text "Flash Movng Fmaily" starts -->
+<section>
+
+</section>
+<!--End of Section -->
+
+<!--Section text "Six Reasons to join the family" starts-->
+<section>
+
+</section>
+<!--End of Section -->
+
+<!--Section text "How much money do you save" starts-->
+<section>
+
+</section>
+<!--End of Section -->
+
+<!--Section text "First Customer and Terms and Conditions" starts-->
+<section>
+
+</section>
+<!--End of Section -->
+
+<hr>
+
+<!--Section text "join the Family" starts-->
+<section>
+
+</section>
+<!--End of Section -->
+
+<!--Section text "We are a part of the family (Reviews)" starts-->
+<section>
+
+</section>
+<!--End of Section -->
+
+<!--Section Instagram starts-->
+<section>
+
+</section>
+<!--End of Section -->
+
+<!--Section Contacts starts-->
+<section>
+
+</section>
+<!--End of Section -->
 @stop
