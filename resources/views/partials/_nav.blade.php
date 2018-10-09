8de8895e13245f1be50bcefb037fc80494327675
Taras <taraskim@wknown.com>
2018-10-08 20:27:16 -0400

    Created Navigation

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,65 @@
-<nav class="navbar navbar-light">
-  <a class="navbar-brand" href="#">
-    <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
-    Bootstrap
-  </a>
+<!-- Top navigation -->
+<div class="container-top top-wrapper" id="hidewhensm">
+  <div class="row" style="margin: 0;">
+    <div class="col-md-4 align-self-center" style="padding: 0;">
+      <div class="top-icons">
+        <div class="icons-padding">
+          <i class="fa fa-facebook fa-icon-top"></i>
+          <i class="fa fa-twitter fa-icon-top"></i>
+          <i class="fa fa-instagram fa-icon-top"></i>
+          <i class="fa fa-yelp fa-icon-top"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4" style="padding: 0;">
+      <div class="top-logo">
+        <a href="/">
+          <img src="/images/logo.png" width="200px"/>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4 align-self-center" style="padding: 0; padding-right: 40px;">
+      <a href="mailto:info@flashmoving.net" class="button-top float-right">info@flashmoving.net</a>
+      <a href="tel:+18572418778" class="button-top float-right">+1 (857) 241-8778</a>
+    </div>
+  </div>
+</div>
+<!-- End of Top Navigation -->
+
+<!-- Main Navigation -->
+<nav class="navbar navbar-expand-lg navbar-our ">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo02">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Services
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Local Moving</a>
+          <a class="dropdown-item" href="#">Interstate Moving</a>
+          <a class="dropdown-item" href="#">Storage</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/prices">Prices</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/family">Family</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about-us">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact-us">Contact us</a>
+      </li>
+    </ul>
+  </div>
 </nav>
+<!-- End of Main Navigation -->
