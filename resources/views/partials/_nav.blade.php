c5df5a8a75d2a094934b56ddcdde5970743bb51f
anton <anton.likhnekevich@gmail.com>
2018-10-12 15:20:02 -0400

    fixes

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -4,10 +4,10 @@
     <div class="col-md-4 align-self-center" style="padding: 0;">
       <div class="top-icons">
         <div class="icons-padding">
-          <i class="fa fa-facebook fa-2x"></i>
-          <i class="fa fa-twitter fa-2x"></i>
-          <i class="fa fa-instagram fa-2x"></i>
-          <i class="fa fa-yelp fa-2x"></i>
+          <a href="https://www.facebook.com/flashmoversma/"><i class="fa fa-facebook fa-2x"></i></a>
+          <a href="https://twitter.com/flashmoversma"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="https://www.instagram.com/flashmoversma/"><i class="fa fa-instagram fa-2x"></i></a>
+          <a href=""><i class="fa fa-yelp fa-2x"></i></a>
         </div>
       </div>
     </div>
