189d494c9ec40a4d0c65a59ff2377f1d1237d92d
anton <anton.likhnekevich@gmail.com>
2018-12-14 14:08:04 -0500

    toggler

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -63,8 +63,8 @@
 <!-- Scrolling Navigation -->
 <nav class="navbar navbar-expand-lg navbar-our-scroll fixed-top" id="scroll-nav" style="height=50px;">
   <a class="navbar-brand" href="#"><img src="/images/logo3.png" width="100px"/></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
