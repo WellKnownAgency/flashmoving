aac115ab0d3c2c13987162d100d30815d5df4728
Taras <taraskim@wknown.com>
2018-12-18 08:22:04 -0500

    initial commit

--- resources/views/local-moving.blade.php
+++ resources/views/local-moving.blade.php
@@ -35,7 +35,7 @@
     <section class="section mb-5">
         <div class="container">
             <div class="section-header">
-                <h2 class="section-title section-title--upper">Services Areas</h2>
+                <h2 class="section-title section-title--upper">Local Moving Services Areas</h2>
             </div>
             <div class="services-collection theme-sm theme-ladder">
                 <div class="services-collection__list">
@@ -155,12 +155,15 @@
                             <div class="col-lg-6 order-lg-0 content-blocks-item__body">
                                 <div class="content-blocks-item__body-inner">
                                     <div class="content-blocks-item__header">
-                                        <div class="content-blocks-item__title-primary">Local Movers (tag #1)</div>
-                                        <div class="content-blocks-item__title-secundare">Seo text</div>
+                                        <h2 class="content-blocks-item__title-primary">Local Movers</h2>
                                     </div>
                                     <div class="content-blocks-item__text">
-                                        <p>This year's Architecture Expo will be held in the fabulous Hong Kong City Hall. The Expo is constantly developing and demonstrating effective cooperation between the development market and the architects. This year's record-breaking edition of the Expo will feature 230 exhibitors and 25 speakers from over 30 countries.</p>
-                                        <p>This year's Architecture Expo will be held in the fabulous Hong Kong City Hall. The Expo is constantly developing and demonstrating effective cooperation between the development market and the architects. This year's record-breaking edition of the Expo will feature 230 exhibitors and 25 speakers from over 30 countries.</p>
+                                      It is extremely exciting when it comes to shift to your new house but at the same time there are a lot of important tasks that you need to undertake as well.
+                                      For instance, the most important task is to hire a local moving company in order to transport your goods.
+                                      Moving your goods and belongings is perhaps the most hectic task that you have to undertake at the time of shifting.
+                                      This is something that you cannot do on your own and therefore, taking professional help becomes mandatory.
+                                      In this case, it is important that you choose a good and reliable moving company who can transfer your goods with care and full security.
+                                      Well, this is where Flash Moving Company comes into the picture. This is one of the best local moving company that you can rely on.
                                     </div>
                                 </div>
                             </div>
@@ -174,12 +177,14 @@
                             <div class="col-lg-6 order-lg-1 content-blocks-item__body">
                                 <div class="content-blocks-item__body-inner">
                                     <div class="content-blocks-item__header">
-                                        <div class="content-blocks-item__title-primary">Local Movers (tag #1)</div>
-                                        <div class="content-blocks-item__title-secundare">Seo text</div>
+                                        <div class="content-blocks-item__title-primary">Flash Moving Services Local</div>
+                                        <div class="content-blocks-item__title-secundare">your trusted local moving service provider</div>
                                     </div>
                                     <div class="content-blocks-item__text">
-                                        <p>This year's Architecture Expo will be held in the fabulous Hong Kong City Hall. The Expo is constantly developing and demonstrating effective cooperation between the development market and the architects. This year's record-breaking edition of the Expo will feature 230 exhibitors and 25 speakers from over 30 countries.</p>
-                                        <p>This year's Architecture Expo will be held in the fabulous Hong Kong City Hall. The Expo is constantly developing and demonstrating effective cooperation between the development market and the architects. This year's record-breaking edition of the Expo will feature 230 exhibitors and 25 speakers from over 30 countries.</p>
+                                      <p>Flash Moving Company is one of the best local moving services that you can get.</p>
+                                      <li>Fully licensed and Insured: Before choosing any moving company it is important to make sure that they have a license and are insured. With Flash Movers you do not have to worry about anything as they have everything under their control.</li>
+
+                                      <li>Clients Love Them: Flash Movers have received over 100 5 star reviews on social media and that too in less than a year. They are known for their excellent moving services and therefore, they make the first choice of every customer when it comes to choosing a local moving company. For them every 3rd customer is a repeat customer.</li>
                                     </div>
                                 </div>
                             </div>
@@ -192,13 +197,12 @@
                             </div>
                             <div class="col-lg-6 order-lg-0 content-blocks-item__body">
                                 <div class="content-blocks-item__body-inner">
-                                    <div class="content-blocks-item__header">
-                                        <div class="content-blocks-item__title-primary">Local Movers (tag #1)</div>
-                                        <div class="content-blocks-item__title-secundare">Seo text</div>
-                                    </div>
                                     <div class="content-blocks-item__text">
-                                        <p>This year's Architecture Expo will be held in the fabulous Hong Kong City Hall. The Expo is constantly developing and demonstrating effective cooperation between the development market and the architects. This year's record-breaking edition of the Expo will feature 230 exhibitors and 25 speakers from over 30 countries.</p>
-                                        <p>This year's Architecture Expo will be held in the fabulous Hong Kong City Hall. The Expo is constantly developing and demonstrating effective cooperation between the development market and the architects. This year's record-breaking edition of the Expo will feature 230 exhibitors and 25 speakers from over 30 countries.</p>
+                                      <li><b>Assembling and Reassembling Furniture:</b> Every member of the Flash moving team is a trained professional. They make use of special tools to perform effective and fast job. By hiring them you don’t have to worry about the safety of your goods as they will take care of everything on their own.</li>
+
+                                      <li><b>Professional and Young team:</b> Flash moving services local are known to have a young team of professionals. They have been in this field for over 5 years now and they are very good in what they do.</li>
+                                      <li><b>They care about your stuff:</b> All your goods will be transported with ultimate care. They care about your stuff more than you. They make use premium quality moving blankets and bannister protections to transport your goods.</li>
+                                      <p>To conclude, it can be said that Flash Moving Company is the best that you can get because of the quality of services that they provide. You can also get their local moving quotes before you assign them for the job.</p>
                                     </div>
                                 </div>
                             </div>
