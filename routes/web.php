93403f879b244ebfb45b8bf6fe0ab23f40a94451
Taras <taraskim@wknown.com>
2019-02-10 15:28:01 -0500

    fixes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/moving-quote', 'PageController@quote');
 Route::post('contact-us', 'PageController@contactUs');
 Route::get('/sitemap.xml', 'PageController@sitemap');
 
+Route::get('/fancy-flowers', function(){
+    return Redirect::to('/case-studies/fancy-flowers', 301);
+});
+
 Route::name('services.')->group(function () {
     Route::get('local-moving/{slug}', 'PageController@localMovingArea')->name('local-moving.area');
     Route::get('local-moving', 'PageController@localMoving')->name('local-moving');
