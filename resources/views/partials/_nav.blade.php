8af580c76bc285c0f75542e884cfc40d9a50c821
Taras <taraskim@wknown.com>
2019-02-17 19:59:42 -0500

    fixes

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -7,7 +7,6 @@
           <a href="https://www.facebook.com/flashmoversma/" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
           <a href="https://twitter.com/flashmoversma" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
           <a href="https://www.instagram.com/flashmoversma/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
-          <a href="https://www.yelp.com/biz/flash-moving-company-newton" target="_blank"><i class="fa fa-yelp fa-2x"></i></a>
         </div>
       </div>
     </div>
